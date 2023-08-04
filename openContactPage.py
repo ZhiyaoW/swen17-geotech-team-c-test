426bb2cf42e03291bb6ed8cea52cb583d65a2c57
ZhiyaoW <110367599+ZhiyaoW@users.noreply.github.com>
2023-08-04 14:57:29 +1000

    correctly open the Contact page

--- openContactPage.py
+++ openContactPage.py
@@ -6,6 +6,6 @@ driver = webdriver.Chrome()
 driver.get('https://swen17-geotech-team-c-frontend.vercel.app/')
 
 time.sleep(3)
-driver.find_element(By.XPATH, "/html/body/div/div/header/ul/li[3]").click()
+driver.find_element(By.XPATH, "/html/body/div/div/header/ul/li[4]").click()
 time.sleep(3)
 driver.close()
--- openHomepage.py
+++ openHomepage.py
@@ -1,10 +1,11 @@
 import time
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+
 driver = webdriver.Chrome()
 
 driver.get('https://swen17-geotech-team-c-frontend.vercel.app/')
 
 time.sleep(3)
-driver.find_element(By.XPATH, "/html/body/div/div/header/ul/li[3]").click()
 driver.close()
+
