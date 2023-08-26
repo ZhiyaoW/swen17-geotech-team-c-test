a513b07e2ec068b1ab93b8c43f2220adc96f18c3
George <zhiyaow1@student.unimelb.edu.au>
2023-08-26 13:07:05 +1000

    update

--- openComponentPage.py
+++ openComponentPage.py
@@ -8,5 +8,6 @@ driver = webdriver.Chrome()
 driver.get('https://swen17-geotech-team-c-frontend.vercel.app/')
 element=WebDriverWait(driver,3,0.5).until(EC.presence_of_element_located(By.XPATH,"/html/body/div/div/header/ul/li[2]"))
 driver.find_element(By.XPATH, "/html/body/div/div/header/ul/li[2]").click()
+
 time.sleep(3)
 driver.close()
