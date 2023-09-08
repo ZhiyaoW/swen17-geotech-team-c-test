418c6195a4c9b4b81dd2de73aa451c432b658d80
ZhiyaoW <110367599+ZhiyaoW@users.noreply.github.com>
2023-09-08 13:40:52 +1000

    Upload the json file from local

---
+++ CalculationFirstComponent.py
@@ -0,0 +1,14 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from  selenium.webdriver.support import expected_conditions as EC
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get('https://swen17-geotech-team-c-frontend.vercel.app/')
+element = WebDriverWait(driver, 3, 0.5).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div/div/aside/div[1]/ul/li[2]")))
+driver.find_element(By.XPATH, "/html/body/div/div/div/div/aside/div[1]/ul/li[2]").click()
+time.sleep(2)
+driver.find_element(By.XPATH, "/html/body/div/div/div/div/aside/div[1]/ul/li[2]/ul/li[2]").click()
+time.sleep(3)
+driver.close()
---
+++ InputJsonFile.py
@@ -0,0 +1,18 @@
+import time
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from  selenium.webdriver.support import expected_conditions as EC
+driver = webdriver.Chrome()
+driver.maximize_window()
+driver.get('https://swen17-geotech-team-c-frontend.vercel.app/')
+element = WebDriverWait(driver, 3, 0.5).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div/div/aside/div[1]/ul/li[2]")))
+driver.find_element(By.XPATH, "/html/body/div/div/div/div/aside/div[1]/ul/li[2]").click()
+time.sleep(2)
+driver.find_element(By.XPATH, "/html/body/div/div/div/div/aside/div[1]/ul/li[2]/ul/li[2]").click()
+time.sleep(3)
+upload_element=driver.find_element(By.XPATH,"/html/body/div[1]/div/div/div/div/main/div/div[1]/div/div/div/div/div/div[1]/div[2]/span/div[1]/span/input")
+file_path = "D:\\formState.json"
+upload_element.send_keys(file_path)
+time.sleep(5)
+driver.close()
